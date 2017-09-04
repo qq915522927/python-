6897c06f563fa94957fe11b172735c59a0623183
wuzhiwen <915522927@qq.com>
2017-09-04 20:27:51 +0800

    真正的递归实现

--- python_study/算法/斐波那契数列的递归实现.py
+++ python_study/算法/斐波那契数列的递归实现.py
@@ -1,7 +1,7 @@
 # coding=utf8
 def fb_list(n,l=[]):
     count = len(l)
-    if count == n:
+    if count == n: #基线条件 （和一般的递归条件有点不一样） 有点不像递归，更像是循环
         return l
     if not count:
         l.append(0)
@@ -9,5 +9,15 @@ def fb_list(n,l=[]):
     l.append(l[-1] + l[-2])
     return fb_list(n,l)
 
+def fb_list2(n): #这个才更像递归
+    if n == 1:
+        return [0]
+    l = fb_list2(n-1)
+    return l.append(l[-1]+l[-2])
+
+
+
 if __name__ == '__main__':
+    print fb_list(10)
+    print '************'
     print fb_list(10)
